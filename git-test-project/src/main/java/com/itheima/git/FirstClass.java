75622380dbccf8e7dfdac26b96ffddee093817fc
wangxu <191261040@qq.com>
2019-02-25 21:38:21 +0800

    first

--- git-test-project/src/main/java/com/itheima/git/FirstClass.java
+++ git-test-project/src/main/java/com/itheima/git/FirstClass.java
@@ -4,4 +4,11 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+
+
+
+    public void hear(){
+        System.out.println(66666);
+    }
 }
